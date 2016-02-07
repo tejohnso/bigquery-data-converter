0ebf653d1cc7d47526150ae3ac16810aa5eb1c8c
Tyler Johnson <mail@tyler-johnson.ca>
2016-02-07 18:14:53 -0500

    Add conversion functionality

--- README.md
+++ README.md
@@ -12,18 +12,18 @@ x-title-3 | y-value-1 | y-value-2 | y-value-3 | ...
 ... | ... | ... | ...  
 ... | ... | ... | ...  
 
+Note that the current row limit is 1000
+
 ### Usage
-Requires *gapi.client* library (eg: [google-client-loader](https://elements.polymer-project.org/elements/google-apis)).  See the test section for a working example.
+Requires *gapi.client* library (eg: [google-client-loader](https://elements.polymer-project.org/elements/google-apis)).  See the test section for a working example.  The element will emit event *new-data* when the data has been converted.
 
 #### Attributes
 Attribute Name | Description | Default
 :---: | :---: | :---:
-projectId | Project id or a selector whose target element *value* contains the project id | "bigquery-projects-selector select"
-datasetId | Dataset id or a selector whose target element *value* contains the dataset id | "bigquery-datasets-selector select"
-tableId | Table id or a selector whose target element *value* contains the table id | "bigquery-tables-selector select"
+projectId | Selector whose target element *value* contains the project id | "bigquery-projects-selector select"
+datasetId | Selector whose target element *value* contains the dataset id | "bigquery-datasets-selector select"
+tableId | Selector whose target element *value* contains the table id | "bigquery-tables-selector select"
 output | Output format | ChartJS Line
-target | Selector which should receive the data | "x-chart"
-attrib | Attribute which should be set on the target element | "config"
 
 ### Development set up
 ```
@@ -49,4 +49,5 @@ npm run teardown-test-env
 
 ### Enhancements
 
+ - Allow direct population of source data or table id rather than using attributes.
  - Emit onchange when data is changed so that target element/attrib isn't the only way to pass on the converted data.
--- bigquery-data-converter.js
+++ bigquery-data-converter.js
@@ -1,14 +1,87 @@
 (function() {
+  const thisElementName = "bigquery-data-converter",
+  defaultAttributes = {
+    projectId: "bigquery-projects-selector select",
+    datasetId: "bigquery-datasets-selector select",
+    tableId: "bigquery-tables-selector select"
+  };
+
   const conv = Object.create(HTMLElement.prototype, {
     createdCallback: {
       value: function() {
+        Object.keys(defaultAttributes).forEach((attributeName)=>{
+          this.setAttribute(attributeName, this.getAttribute(attributeName) || defaultAttributes[attributeName]);
+        });
+
+        var sourceEl = document.querySelector(this.getAttribute("tableId"));
+        if (sourceEl) {
+          sourceEl.addEventListener("change", convert);
+        }
       }
     },
-    addtributeChangedCallback {
+    attributeChangedCallback: {
       value: function (name, oldVal, newVal) {
+        if (name !== "tableId") {return;}
+
+        var oldEl = document.querySelector(oldVal);
+        var newEl = document.querySelector(newVal);
+
+        if (oldEl) {
+          oldEl.removeEventListener("change", convert);
+        }
+
+        if (newEl) {
+          newEl.addEventListener("change", convert);
+        } 
       }
     }
   });
 
-  document.registerElement("bigquery-data-converter", {prototype: conv});
+  document.registerElement(thisElementName, {prototype: conv});
+
+  function convert() {
+    var el = document.querySelector(thisElementName);
+
+    gapi.client.bigquery.tabledata.list({
+      projectId: document.querySelector(el.getAttribute("projectId")).value,
+      datasetId: document.querySelector(el.getAttribute("datasetId")).value,
+      tableId: document.querySelector(el.getAttribute("tableId")).value,
+      maxResults: 1000
+    })
+    .then((resp)=>{
+      el.dispatchEvent(new CustomEvent("new-data", chartJsLine()));
+    });
+  }
+
+  function chartJsLine(data) {
+    return {
+      "detail": { "data":
+        {
+          "labels": ["January", "February", "March", "April", "May", "June", "July"],
+          "datasets": [
+          {
+            "label": "My First dataset",
+            "fillColor": "rgba(220,220,220,0.2)",
+            "strokeColor": "rgba(220,220,220,1)",
+            "pointColor": "rgba(220,220,220,1)",
+            "pointStrokeColor": "#fff",
+            "pointHighlightFill": "#fff",
+            "pointHighlightStroke": "rgba(220,220,220,1)",
+            "data": [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            "label": "My Second dataset",
+            "fillColor": "rgba(151,187,205,0.2)",
+            "strokeColor": "rgba(151,187,205,1)",
+            "pointColor": "rgba(151,187,205,1)",
+            "pointStrokeColor": "#fff",
+            "pointHighlightFill": "#fff",
+            "pointHighlightStroke": "rgba(151,187,205,1)",
+            "data": [28, 48, 40, 19, 86, 27, 90]
+          }
+          ]
+        }
+      }
+    };
+  }
 }());
